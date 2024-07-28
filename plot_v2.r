ffdaa90a739913fb2c3448fcdb68354beec53b93
gratidutra <gratirodrigues.gdr@gmail.com>
2024-07-28 13:32:29 -0300

    new version plot

--- plot_v2.r
+++ plot_v2.r
@@ -11,7 +11,7 @@ df <-
 
 cases_by_city <- df %>%
   group_by(mun= municipio)%>%
-  summarise(n= n_distinct(ID_do_caso)) %>%
+  summarise(`Number of cadavers`= n_distinct(ID_do_caso)) %>%
   mutate(mun = tolower(mun))
 #        n = case_when(n>0 ~ 1))
 
@@ -22,18 +22,51 @@ mun <- mun %>%
 
 sf_with_cases <- mun %>%
   left_join(cases_by_city, by = c("name_muni_2" = "mun")) %>% 
-  replace_na(list(n = 0)) 
+  replace_na(list(n = 0))
 
-mapa_casos <- tm_shape(sf_with_cases) + 
-  tm_fill(col ='n', title= expression("Número de cadáveres"), palette = 'Blues') +
-  tm_borders()+
-  tm_compass(type = '8star', position = c("right", "top"), size = 2.5) 
-  #tm_scale_bar(breaks = c(0,50,100), text.size = 0.5, position = c("right", "bottom"))
+# 1 -----------------------------------------------------------------------
 
-tmap_mode("view")
+
+mapa_casos <- tm_shape(sf_with_cases) +
+  tm_polygons(fill = 'n', col = NA, fill.legend = tm_legend(title = expression("Número de cadáveres"), palette = 'Blues')) +
+  tm_borders() +
+  tm_compass(type = '8star', position = c("left", "top"), size = 2.5) +
+  tm_scalebar(breaks = c(0, 50, 100), text.size = 0.5, position = c("right", "bottom")) +
+  tm_minimap()
+  
+
+#tmap_mode("view")
 mapa_casos
 
+# 2 -----------------------------------------------------------------------
+brasil <- st_read("data/shapefiles/BR/BR_UF_2022.shp")
 
+main_map <- ggplot(data = sf_with_cases) +
+  geom_sf(aes(fill = `Number of cadavers`)) +
+  scale_fill_gradient(low = "#fdf0d5", high = "red", na.value = 'lightgray') +
+  #geom_sf(color = "black") +
+  annotation_north_arrow(
+    location = "tl", which_north = "true", 
+    style = north_arrow_fancy_orienteering,
+    height = unit(2.5, "cm")
+  ) +
+  annotation_scale(location = "br", width_hint = 0.5) +
+  theme_minimal() +
+  theme(legend.position = "right")
+
+# Criar o minimapa
+inset_map <- ggplot(data = brasil) +
+  geom_sf() +
+  theme_void()
+
+# Combinar os mapas
+combined_map <- ggdraw() +
+  draw_plot(main_map) +
+  draw_plot(inset_map, x = 0.7, y = 0.7, width = 0.25, height = 0.25)
+
+combined_map
+
+ggsave('new_map.svg', height=10, width = 10, dpi=600)
 # casos ------------------------------------------------------------
 
 casos <- read_csv('casos.csv')
